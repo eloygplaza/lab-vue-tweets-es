52d13a13a785af86d9134155ea401de6e1973366
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-12 17:31:12 +0200

    iteration 5 done

---
+++ src/components/User.vue
@@ -0,0 +1,20 @@
+<!-- START TEMPLATE -->
+<template>
+    <span class="user">
+        <span class="name">{{props.username}}</span>
+        <span class="handle">{{props.handle}}</span>
+    </span>
+
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+
+    const props = defineProps(['username','handle']);
+    
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
