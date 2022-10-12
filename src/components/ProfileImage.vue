e0fe74411fb9b403cd2dbc4cefb6c72b959d63dd
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-12 17:31:02 +0200

    iteration 4 done

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<!-- START TEMPLATE -->
+<template>
+    
+    <img :src="props.src" class="profile" alt="profile" tweet=""/>
+
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+    import { defineProps } from 'vue';
+    
+    const props = defineProps(['src']);
+
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
