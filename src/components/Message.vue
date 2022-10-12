e2cda1ff525171b86368f10372a70a5a6d62a509
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-12 17:31:35 +0200

    iteration 7 done

---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<!-- START TEMPLATE -->
+<template>
+
+    <p class="message">
+        {{props.text}}
+    </p>
+
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps(['text']);
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
