d6943b1d1aac39f4739c38fd3f1c652e942021d4
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-12 17:31:23 +0200

    iteration 6 done

---
+++ src/components/Timestamp.vue
@@ -0,0 +1,18 @@
+<!-- START TEMPLATE -->
+<template>
+
+    <span class="timestamp">{{props.timestamp}}</span>
+
+</template>
+<!-- END TEMPLATE -->
+<!-- START SCRIPT -->
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps(['timestamp']);
+</script>
+<!-- END SCRIPT -->
+<!-- START STYLE -->
+<style scoped>
+</style>
+<!-- END STYLE -->
